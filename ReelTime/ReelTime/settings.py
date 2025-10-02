6d14ea24c3839374706a0c75e59112f68f74cafa
therean-07 <johntiklo7@gmail.com>
2025-10-02 09:18:01 +0800

    feature(reeltime): database_setup

--- ReelTime/ReelTime/settings.py
+++ ReelTime/ReelTime/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +80,11 @@ WSGI_APPLICATION = 'ReelTime.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///dbsqlite3",
+        conn_max_age=600, #persist the connection for 10 minutes
+        ssl_require=True # Enforce SSL connection
+    )
 }
 
 
